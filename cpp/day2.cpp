67205141215ce0f5954b9e6185542200f1c0c4a8
KenethSandoval <kenethra.74@gmail.com>
2021-12-06 21:54:48 -0600

    (feat: cpp) day2

---
+++ cpp/day2.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+enum class Action { Forward, Up, Down };
+
+auto main() -> int {
+
+  return 0;
+}
