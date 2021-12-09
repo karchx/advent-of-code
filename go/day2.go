6a792ca89f2bc500322a3f8761fd9fc2a0059b0d
KenethSandoval <kenethra.74@gmail.com>
2021-12-08 21:18:35 -0600

    (feat:golang) day2 part2

--- go/day2.go
+++ go/day2.go
@@ -14,19 +14,7 @@ type inst struct {
 	val int
 }
 
-func main() {
-	result := utils.ReadFile2("../inputzz/day2.txt")
-
-	var ins []inst
-	for _, s := range result {
-		split := strings.Split(s, " ")
-		v, err := strconv.Atoi(split[1])
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		ins = append(ins, inst{split[0], v})
-	}
-
+func partOne(ins []inst) {
 	h := 0
 	d := 0
 
@@ -42,5 +30,47 @@ func main() {
 			log.Fatal(i)
 		}
 	}
+
+  fmt.Println("Part 1")
 	fmt.Println(h, d, h*d)
+}
+
+func partTwo(ins []inst) {
+	h := 0
+	d := 0
+	z := 0
+
+	for _, i := range ins {
+		switch i.dir {
+		case "forward":
+			h += i.val
+			d += (i.val * z)
+		case "up":
+			z -= i.val
+		case "down":
+			z += i.val
+		default:
+			log.Fatal(i)
+		}
+	}
+
+  fmt.Println("Part 2")
+	fmt.Println(h, d, z, h*d)
+}
+
+func main() {
+	result := utils.ReadFile2("../inputzz/day2.txt")
+
+	var ins []inst
+	for _, s := range result {
+		split := strings.Split(s, " ")
+		v, err := strconv.Atoi(split[1])
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		ins = append(ins, inst{split[0], v})
+	}
+
+  partOne(ins)
+  partTwo(ins)
 }
