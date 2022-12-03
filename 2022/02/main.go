d95763512a8625313951df9981883bb02f4ffebd
Стив <kenethra.74@gmail.com>
2022-12-03 15:09:06 -0600

    day02: part2 done

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -18,10 +18,19 @@ type Move struct {
 	score        int
 }
 
-func TotalScoreWithELfStrategy(tee io.Reader) int {
+type Move2 struct {
+	opponentMove string
+	win          int
+	draw         int
+	loss         int
+}
 
-	win := 6
-	draw := 3
+const (
+	win  = 6
+	draw = 3
+)
+
+func TotalScoreWithELfStrategy(tee io.Reader) int {
 
 	rock := Move{"A", "X", "C", "B", 1}
 	paper := Move{"B", "Y", "A", "C", 2}
@@ -68,6 +77,54 @@ func TotalScoreWithELfStrategy(tee io.Reader) int {
 	return sum
 }
 
+func TotalScoreWithELfStrategyWinLose(tee io.Reader) int {
+	rockScore := 1
+	paperScore := 2
+	scissorsScore := 3
+
+	rock := Move2{"A", paperScore, rockScore, scissorsScore}
+	paper := Move2{"B", scissorsScore, paperScore, rockScore}
+	scissors := Move2{"C", rockScore, scissorsScore, paperScore}
+
+	sum := 0
+
+	input := bufio.NewScanner(tee)
+	input.Split(bufio.ScanLines)
+
+	// for i := 0; i < len(input)-1; i++ {
+	for input.Scan() {
+		moves := strings.Split(input.Text(), " ")
+
+		opponentMove := moves[0]
+		outCome := moves[1]
+
+		var opponentMoveType Move2
+		switch opponentMove {
+		case "A":
+			opponentMoveType = rock
+		case "B":
+			opponentMoveType = paper
+		case "C":
+			opponentMoveType = scissors
+		default:
+			fmt.Println("Uhh something is not right here, check your input.txt")
+		}
+
+		switch outCome {
+		case "X":
+			sum += opponentMoveType.loss
+		case "Y":
+			sum += opponentMoveType.draw + draw
+		case "Z":
+			sum += opponentMoveType.win + win
+		default:
+			fmt.Println("Uhh something is not right here, check your input.txt")
+		}
+	}
+
+  return sum
+}
+
 func main() {
 	f, err := os.Open("../inputs/input-02")
 	if err != nil {
@@ -78,6 +135,10 @@ func main() {
 
 	var buf bytes.Buffer
 	tee := io.TeeReader(f, &buf)
+
 	sum := TotalScoreWithELfStrategy(tee)
-	fmt.Println(sum)
+	fmt.Printf("Part 01: %d\n", sum)
+
+	sum2 := TotalScoreWithELfStrategyWinLose(&buf)
+	fmt.Printf("Part 02: %d\n", sum2)
 }
