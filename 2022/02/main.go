24e0cf92c7fa1cc2a32634d583da7c3e69405f60
Стив <kenethra.74@gmail.com>
2022-12-03 14:26:16 -0600

    day02: part1 done

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -7,162 +7,69 @@ import (
 	"io"
 	"log"
 	"os"
-	"strconv"
+	"strings"
 )
 
-type Option int64
-type Result int64
-
-const (
-	Rock Option = iota
-	Paper
-	Scissors
-)
-
-const (
-	Lost Result = iota
-	Draw
-	Win
-)
-
-func parsePlayOpponent(input string) Option {
-	switch input {
-	case "A", "X":
-		return Rock
-	case "B", "Y":
-		return Paper
-	case "C", "Z":
-		return Scissors
-	}
-	return 0
+type Move struct {
+	opponentMove string
+	yourMove     string
+	win          string
+	loss         string
+	score        int
 }
 
-func parseResult(input string) Result {
-	switch input {
-	case "X":
-		return Lost
-	case "Y":
-		return Draw
-	case "Z":
-		return Win
-	}
-	return 0
-}
+func TotalScoreWithELfStrategy(tee io.Reader) int {
 
-func (option Option) play(opponent Option) Result {
-	if option == opponent {
-		return Draw
-	}
+	win := 6
+	draw := 3
 
-	if option.prev() == opponent {
-		return Win
-	}
+	rock := Move{"A", "X", "C", "B", 1}
+	paper := Move{"B", "Y", "A", "C", 2}
+	scissors := Move{"C", "Z", "B", "A", 3}
 
-	return Lost
-}
-
-func (option Option) getScore() int {
-	switch option {
-	case Rock:
-		return 1
-	case Paper:
-		return 2
-	case Scissors:
-		return 3
-	}
-	return 0
-}
-
-func (result Result) getScore() int {
-	switch result {
-	case Lost:
-		return 0
-	case Draw:
-		return 3
-	case Win:
-		return 6
-	}
-	return 0
-}
-
-func (option Option) next() Option {
-	if option > 1 {
-		return 0
-	}
-	return option + 1
-}
-
-func (option Option) prev() Option {
-	if option < 1 {
-		return 2
-	}
-	return option - 1
-}
-
-func (result Result) choosePlay(opponent Option) Option {
-	if result == Draw {
-		return opponent
-	}
-
-	if result == Win {
-		return opponent.next()
-	}
-
-	if result == Lost {
-		return opponent.prev()
-	}
-
-	return 0
-}
-
-func TotalScoreWithELfStrategy(reader io.Reader) int {
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanLines)
 	sum := 0
 
-	for scanner.Scan() {
-		var opponent, player string
-		_, err := fmt.Sscanf(scanner.Text(), "%s %s", &opponent, &player)
-		if err != nil {
-			return 0
+	input := bufio.NewScanner(tee)
+	input.Split(bufio.ScanLines)
+
+	// for i := 0; i < len(input)-1; i++ {
+	for input.Scan() {
+		moves := strings.Split(input.Text(), " ")
+
+		opponentMove := moves[0]
+		yourMove := moves[1]
+
+		var yourMoveType Move
+		switch yourMove {
+		case "X":
+			yourMoveType = rock
+		case "Y":
+			yourMoveType = paper
+		case "Z":
+			yourMoveType = scissors
+		default:
+			fmt.Println("Uhh something is not right here, check your input.txt")
 		}
 
-		optionOpponent := parsePlayOpponent(opponent)
-		optionPlayer := parsePlayOpponent(opponent)
-
-		result := optionPlayer.play(optionOpponent)
-
-		sum += result.getScore() + optionPlayer.getScore()
-
-	}
-	return sum
-}
-
-func TotalScoreWithElfStrategyWinLose(input io.Reader) int {
-	scanner := bufio.NewScanner(input)
-	scanner.Split(bufio.ScanLines)
-
-	sum := 0
-	for scanner.Scan() {
-		var opponent, winLoseOrDraw string 
-		_, err := fmt.Sscanf(scanner.Text(), "%s %s", &opponent, &winLoseOrDraw)
-		if err != nil {
-			return 0
+		switch opponentMove {
+		case yourMoveType.opponentMove:
+			sum += draw
+		case yourMoveType.win:
+			sum += win
+		case yourMoveType.loss:
+			sum += 0
+		default:
+			fmt.Println("Uhh something is not right here, check your input.txt")
 		}
 
-		opponentPlay := parsePlayOpponent(opponent)
-		desiredResult := parseResult(winLoseOrDraw)
-
-		playerPlay := desiredResult.choosePlay(opponentPlay)
-
-		sum += desiredResult.getScore() + playerPlay.getScore()
+		sum += yourMoveType.score
 	}
 
 	return sum
 }
 
 func main() {
-	f, err := os.Open("../inputs/input-test-02")
+	f, err := os.Open("../inputs/input-02")
 	if err != nil {
 		log.Fatal("failed to open file")
 	}
@@ -171,6 +78,6 @@ func main() {
 
 	var buf bytes.Buffer
 	tee := io.TeeReader(f, &buf)
-  got := TotalScoreWithELfStrategy(tee)
-	fmt.Println(strconv.Itoa(got))
+	sum := TotalScoreWithELfStrategy(tee)
+	fmt.Println(sum)
 }
